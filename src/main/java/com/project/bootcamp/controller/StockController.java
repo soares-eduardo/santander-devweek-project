c56d3edbde87f48644a1bfb8bfa3033e9797a6d9
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-05-31 09:53:21 -0300

    Post request funcionando. Adicionei camadas Service e Repository. Falta regra de negocio.

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -7,7 +7,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import com.project.bootcamp.model.dto.StockDTO;
+import com.project.bootcamp.service.StockService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,9 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/stock")
 public class StockController {
 
+    @Autowired
+    private StockService service;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto) {   
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<StockDTO> save(@Valid @RequestBody StockDTO dto) {
+        return ResponseEntity.ok(service.save(dto));
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/com/project/bootcamp/model/Stock.java
+++ src/main/java/com/project/bootcamp/model/Stock.java
@@ -1,32 +1,34 @@
-package com.project.bootcamp.model.dto;
+package com.project.bootcamp.model;
 
 import java.time.LocalDate;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+@Entity
+@Table(name = "tb_stock")
+public class Stock {
 
-public class StockDTO {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
 
-    @NotNull
+    @Column(name = "name")
     private String name;
 
-    @NotNull
-    @DecimalMin(value = "0.00")
-    @Digits(integer = 6, fraction = 2)
+    @Column(name = "price")
     private Double price;
 
-    @NotNull
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    private LocalDate date;
-
-    @NotNull
-    @Digits(integer = 3, fraction = 2)
+    @Column(name = "variation")
     private Double variation;
+
+    @Column(name = "date")
+    private LocalDate date;
 
     public Long getId() {
         return id;
@@ -40,12 +42,12 @@ public class StockDTO {
         return price;
     }
 
-    public LocalDate getDate() {
-        return date;
-    }
-
     public Double getVariation() {
         return variation;
+    }
+
+    public LocalDate getDate() {
+        return date;
     }
 
     public void setId(Long id) {
@@ -60,11 +62,11 @@ public class StockDTO {
         this.price = price;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
     public void setVariation(Double variation) {
         this.variation = variation;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
